339273c36de779f5edf02686c5490728d4fd80c9
20200694kit <81212690+20200694kit@users.noreply.github.com>
2021-03-30 16:43:24 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+
+    
+block = ["ㄹ","ㄱ","ㄴ","ㅁ","ㅣ","ㅜ","q"]
+print(block)
+print("블록 갯수:", len(block))
+
+for i in range(len(block)):
+    print(block[i])
+
+#print(block[0])
+#print(block[1])
+#print(block[2])
+#print(block[3])
+#print(block[4])
+
+#for x in block:
+#   print(x)
